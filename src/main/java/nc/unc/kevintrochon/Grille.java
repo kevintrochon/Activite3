abc9ecaedaf681de8e4ec15b23dceadcc422d574
kevin-trochon <trochonkevin@gmail.com>
2021-12-21 16:30:01 +1100

    Correction des rapports dans le fichier CaractereInterditException et de l interface Grille

--- src/main/java/nc/unc/kevintrochon/Grille.java
+++ src/main/java/nc/unc/kevintrochon/Grille.java
@@ -4,31 +4,37 @@ package nc.unc.kevintrochon;
  * Interface pour la contruction d'une grille.
  */
 public interface Grille {
-  /** Caractere de case vide */
+  /** Caractere de case vide. */
   char EMPTY = '@';
 
 
-  /** @return largeur/hauteur de la grille */
-  int getDimension();
   /**
-   * Affecte une valeur dans la grille
-   * @param x position x dans la grille
-   * @param y position y dans la grille
-   * @param value valeur a mettre dans la case
-   * @throw HorsBornesException si x ou y sont hors bornes (0-8)
-   * @throw ValeurImpossibleException si la valeur est interdite aux vues des autres valeurs de la grille
-   * @throw CaractereInterditException si value n'est pas un caractere autorise ('1',...,'9')
+   * Method qui donne la dimension de la grille.
    */
-  void setValue(int x, int y, char value) throws HorsBornesException, ValeurImpossibleException,
+  int getDimension();
+
+  /**
+   * Affecte une valeur dans la grille.
+   * @param ligne position x dans la grille
+   * @param colonne position y dans la grille
+   * @param value valeur a mettre dans la case
+   * @throw HorsBornesException
+   *  si x ou y sont hors bornes (0-8)
+   * @throw ValeurImpossibleException
+   *  si la valeur est interdite aux vues des autres valeurs de la grille
+   * @throw CaractereInterditException
+   *  si value n'est pas un caractere autorise ('1',...,'9')
+   */
+  void setValue(int ligne, int colonne, char value) throws HorsBornesException, ValeurImpossibleException,
       CaractereInterditException;
   /**
    * Recupere une valeur de la grille
-   * @param x position x dans la grille
-   * @param y position y dans la grille
+   * @param ligne position x dans la grille
+   * @param colonne position y dans la grille
    * @return valeur dans la case x,y
    * @throw HorsBornesException si x ou y sont hors bornes (0-8)
    */
-  char getValue(int x, int y) throws HorsBornesException;
+  char getValue(int ligne, int colonne) throws HorsBornesException;
   /**
    * Test si une grille est terminee
    * @return true si la grille est complete
@@ -37,11 +43,11 @@ public interface Grille {
   /**
    * Test si une valeur est possible dans la grille par rapport a ce qu'elle
    * contient deja
-   * @param x position x dans la grille
-   * @param y position y dans la grille
+   * @param ligne position x dans la grille
+   * @param colonne position y dans la grille
    * @param value valeur a mettre dans la case
    * @throw HorsBornesException si x ou y sont hors bornes (0-8)
    * @throw CaractereInterditException si value n'est pas un caractere autorise ('1', ..., '9', ...)
    */
-  boolean possible(int x, int y, char value) throws HorsBornesException, CaractereInterditException;
+  boolean possible(int ligne, int colonne, char value) throws HorsBornesException, CaractereInterditException;
 }
